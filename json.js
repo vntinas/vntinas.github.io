0e479228d3225aec13e02cdafb175e941f5fb43d
Vasileios Ntinas <42007421+vntinas@users.noreply.github.com>
2021-02-13 21:04:07 +0200

    Delete json.js

--- json.js
+++
@@ -1,9 +0,0 @@
-$().ready(function(){
-    $.getJSON( "/data.json", function( data ) {
-    console.log(data);
-    
-    for (x in data) {
-        $("#text").html(x["firstName"]);
-    }
-  });
-});
